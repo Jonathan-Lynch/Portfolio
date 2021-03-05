753091cb07b5b3b85c2bc6399fe533887faa8209
Jonathan-Lynch <jonathanlynch2001@gmail.com>
2021-03-05 17:07:49 -0600

    move files around and added animate css

---
+++ js/loops.js
@@ -0,0 +1,16 @@
+function js()  {
+    for (var i = 0; i < 5; i++)
+    {
+        console.log(i)
+    }
+    
+    console.log("Hello");
+}
+
+let unsortedArr = [10, 2, 6, 15, 1, 17, 8, 13];
+
+let swapped;
+
+function bubbleSort(arr) {
+    let end = arr.length - 1;
+}
